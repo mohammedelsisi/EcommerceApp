3608636c93855374696fbd94a8e4e93d0abe3e4e
yasmina <yasminaabdelazim@gmail.com>
2021-04-03 14:42:38 +0200

    ajax for address page

---
+++ src/main/webapp/layout/js/addressScript.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict'
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+function appendAddress() {
+
+    var node = document.createElement("LI");
+    var nodevalue =  document.getElementById("exampleInputAddress").value;
+    node.innerHTML = ('<input type="radio" id="address1" name="address" style="margin-top: 10%"> <label for="address1">' + nodevalue + '</label><!-- Divider --><hr class="sidebar-divider my-0">');
+
+    document.getElementById("Addresses").appendChild(node);
+
+    console.log(nodevalue);
+    $.ajax({
+        url: "Addresses",
+        method: "POST",
+        dataType: "html",
+        data: {
+            "Address": nodevalue
+        }
+
+    })
+
+}
+
+
+function  getAddress(){
+
+
+
+}
+
+
